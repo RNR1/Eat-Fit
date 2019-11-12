fe2386370e181ebdd2f52bf4ebb5fe059aa4538a
yanivsultan <yanivsultan5@gmail.com>
2019-11-12 19:08:05 +0200

    start working on totalCal Counter

--- dist/Renderer.js
+++ dist/Renderer.js
@@ -1,17 +1,19 @@
 class Renderer {
-    renderFood(foods) {
+    renderFood(foods, cal) {
         const source = $('#food-template').html();
         const template = Handlebars.compile(source);
-        let newHTML = template( foods );
+        let newHTML = template(foods);
         $("#food-container").empty().append(newHTML);
-    
+        if (cal)
+            $("#totalCalMessage").empty().append(`The Total Calories you have in your menu is: ${cal}`);
+
     }
 
     renderMenu(menu) {
         const source = $('#menu-template').html();
         const template = Handlebars.compile(source);
-        let newHTML = template( {menu} );
+        let newHTML = template({ menu });
         $("#menu-container").empty().append(newHTML);
-    
+
     }
 }
--- dist/models/Menu.js
+++ dist/models/Menu.js
@@ -1,17 +1,20 @@
 class Menu {
     constructor() {
         this.menuData = []
-
+        this.totalCal = 0
         
     }
 
     addToMenu(food) {
         this.menuData.push(food)
+        this.totalCal += food.cal
     }
 
     removeFromMenu(food) {
         let foodToDelete = this.menuData.findIndex(f => f.name === food)
+        console.log(foodToDelete)
         this.menuData.splice(foodToDelete,1)
+        // this.totalCal -= food.cal
         
     }
 
