4541cef57323d0aaa4128ebd1047ac24cc3c681b
Ron Braha <Ron.Braha@icloud.com>
2019-11-13 16:29:22 +0200

    error handling search

--- dist/main.js
+++ dist/main.js
@@ -4,40 +4,47 @@ let menu = new Menu()
 let user = new User("Yaniv", 2500)
 const renderer = new Renderer()
 
-$('#searchButton').on('click', async function () {
-    let input = $('#searchFood').val()
-    await user.getFood(input)
-    renderer.renderFood(user.foodData)
+$("#searchButton").on("click", async function() {
+	let input = $("#searchFood").val()
+	if (input === "") {
+		return
+	}
+	await user.getFood(input)
+	if (!user.foodData) {
+		return
+	}
+	renderer.renderFood(user.foodData)
 })
 
-
-$("#food-container").on("click", ".addToMenuButton", function () {
-    let foodName = $(this).closest(".Food").find(".foodName").text()
-    let meal = $("#meals").val()
-    if (user.foodData.name === foodName) {
-        menu.addToMenu(user.foodData)
-        renderer.renderMenu(menu[meal], meal)
-        renderer.renderFood(user.foodData)
-        renderer.renderNutrients(menu.nutrients)
-        //Show message to user that it`s added
-    } else {
-        console.log("Problem, Big One.")
-    }
-
-
+$("#food-container").on("click", ".addToMenuButton", function() {
+	let foodName = $(this)
+		.closest(".Food")
+		.find(".foodName")
+		.text()
+	let meal = $("#meals").val()
+	if (user.foodData.name === foodName) {
+		menu.addToMenu(user.foodData)
+		renderer.renderMenu(menu[meal], meal)
+		renderer.renderFood(user.foodData)
+		renderer.renderNutrients(menu.nutrients)
+		//Show message to user that it`s added
+	} else {
+		console.log("Problem, Big One.")
+	}
 })
 
-
-$("#food-container").on("click", ".removeFromMenuButton", function () {
-    let foodName = $(this).closest(".Food").find(".foodName").text()
-    if (user.foodData.name === foodName) {
-        menu.removeFromMenu(user.foodData)
-        renderer.renderFood(user.foodData)
-        renderer.renderNutrients(menu.nutrients)
-        //Show message to user that it`s removed
-    }
+$("#food-container").on("click", ".removeFromMenuButton", function() {
+	let foodName = $(this)
+		.closest(".Food")
+		.find(".foodName")
+		.text()
+	if (user.foodData.name === foodName) {
+		menu.removeFromMenu(user.foodData)
+		renderer.renderFood(user.foodData)
+		renderer.renderNutrients(menu.nutrients)
+		//Show message to user that it`s removed
+	}
 })
-
 
 // //for the second part of the project. comment it out for now.
 // let user = new User("Yaniv", 2500)
--- dist/models/User.js
+++ dist/models/User.js
@@ -1,57 +1,64 @@
 class User {
-    constructor(id, name, bmr) {
-        this.id = id
-        this.name = name
-        this.bmr = bmr
-        this.dailyMenu = []
-        this.foodData = {}
-    }
+	constructor(id, name, bmr) {
+		this.id = id
+		this.name = name
+		this.bmr = bmr
+		this.dailyMenu = []
+		this.foodData = {}
+	}
 
-    async getFood(foodName) {
-        try {
-        this.foodData = await $.get(`/food/${foodName}`)
-        }
-        catch (err) {
-            return err
-        }
-    }
+	async getFood(foodName) {
+		let data
+		try {
+			data = await $.get(`/food/${foodName}`)
+			if (!data.name) {
+				throw new Error("Food not found")
+			}
+		} catch (err) {
+			return (this.foodData = undefined)
+		}
+		this.foodData = data
+	}
 
-    async consumeFood(foodId,meal) {
-        await $.ajax({
-            url: `/consume`,
-            type: 'PUT',
-            data: {userId: this.id, foodId: foodId,
-                    meal: meal},
-            success: function(result) {
-                console.log("Food Consumed")
-            }
-        })
-    }
+	async consumeFood(foodId, meal) {
+		await $.ajax({
+			url: `/consume`,
+			type: "PUT",
+			data: { userId: this.id, foodId: foodId, meal: meal },
+			success: function(result) {
+				console.log("Food Consumed")
+			}
+		})
+	}
 
-    async getDailyMenu() {
-        this.dailyMenu = await $.get(`/menu/${this.userId}`)
-    }
+	async getDailyMenu() {
+		this.dailyMenu = await $.get(`/menu/${this.userId}`)
+	}
 
-    async createMenu() {
-        if (this.dailyMenu.length === 0) {
-            console.log("The menu is empty")
-            //show message to user.
-        } else {
-            
-            await $.post(`/user/menu`, {userId: this.id, menuId: this.dailyMenu.id})
-        }
-    }
+	async createMenu() {
+		if (this.dailyMenu.length === 0) {
+			console.log("The menu is empty")
+			//show message to user.
+		} else {
+			await $.post(`/user/menu`, {
+				userId: this.id,
+				menuId: this.dailyMenu.id
+			})
+		}
+	}
 
-    async removeMenu(){
-        
-        await $.ajax({
-            url: `/menu`,
-            type: 'DELETE',
-            data: {userId: this.userId, selectedDay: this.dailyMenu.dayInWeek},
-            success: function(result) {
-                this.dailyMenu = []
-                console.log("Menu Deleted")
-            }
-        })
-    }
+	async removeMenu() {
+		await $.ajax({
+			url: `/menu`,
+			type: "DELETE",
+			data: {
+				userId: this.userId,
+				selectedDay: this.dailyMenu.dayInWeek
+			},
+			success: function(result) {
+				this.dailyMenu = []
+				console.log("Menu Deleted")
+			}
+		})
+	}
 }
